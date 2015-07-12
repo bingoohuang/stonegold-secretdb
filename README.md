8d0664c9fabca62178521b83b28df380fa20a2b4
bingoohuang <bingoohuang@promote.cache-dns.local>
2015-07-12 15:40:26 +0800

    use try-with-resources

--- README.md
+++ README.md
@@ -42,3 +42,31 @@ a demonstration for auto encryption/decryption of db table secret fields in inte
     statement.execute(dropTableSql);
     statement.close();
     ```
+
+1. use try-with-resources statement
+
+    ```java
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+
+        try (Connection conn = DriverManager.getConnection("jdbc:h2:~/stonegold", "sa", "")) {
+            String dropTableSql = "drop table if exists person";
+            try (Statement statement = conn.createStatement()) {
+                statement.execute(dropTableSql);
+            }
+
+            String createTableSql = "create table  person(name varchar(10), id_no varchar(18), credit_card varchar(16))";
+            try (PreparedStatement ps = conn.prepareStatement(createTableSql)) {
+                ps.execute();
+            }
+
+            String insertSql = "insert into person(name, id_no, credit_card) values(?, ?, ?)";
+            try (PreparedStatement insertPs = conn.prepareStatement(insertSql)) {
+                insertPs.setString(1, "bingoo");
+                insertPs.setString(2, "321421198312111234");
+                insertPs.setString(3, "1111222233334444");
+                insertPs.execute();
+            }
+        }
+    }
+    ```
--- src/main/java/stonegold/Main.java
+++ src/main/java/stonegold/Main.java
@@ -5,26 +5,25 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.h2.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:h2:~/stonegold", "sa", "");
 
-        String dropTableSql = "drop table if exists person";
-        Statement statement = conn.createStatement();
-        statement.execute(dropTableSql);
-        statement.close();
+        try (Connection conn = DriverManager.getConnection("jdbc:h2:~/stonegold", "sa", "")) {
+            String dropTableSql = "drop table if exists person";
+            try (Statement statement = conn.createStatement()) {
+                statement.execute(dropTableSql);
+            }
 
-        String createTableSql = "create table  person(name varchar(10), id_no varchar(18), credit_card varchar(16))";
-        PreparedStatement ps = conn.prepareStatement(createTableSql);
-        ps.execute();
-        ps.close();
+            String createTableSql = "create table  person(name varchar(10), id_no varchar(18), credit_card varchar(16))";
+            try (PreparedStatement ps = conn.prepareStatement(createTableSql)) {
+                ps.execute();
+            }
 
-        String insertSql = "insert into person(name, id_no, credit_card) values(?, ?, ?)";
-        PreparedStatement insertPs = conn.prepareStatement(insertSql);
-        insertPs.setString(1, "bingoo");
-        insertPs.setString(2, "321421198312111234");
-        insertPs.setString(3, "1111222233334444");
-        insertPs.execute();
-        insertPs.close();
-
-        conn.close();
+            String insertSql = "insert into person(name, id_no, credit_card) values(?, ?, ?)";
+            try (PreparedStatement insertPs = conn.prepareStatement(insertSql)) {
+                insertPs.setString(1, "bingoo");
+                insertPs.setString(2, "321421198312111234");
+                insertPs.setString(3, "1111222233334444");
+                insertPs.execute();
+            }
+        }
     }
 }
