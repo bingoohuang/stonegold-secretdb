a7858aff89ca5b67c339d2a9c984107a86ad9ce1
bingoohuang <bingoohuang@bingoohuangdeMacBook-Pro.local>
2015-07-13 17:58:08 +0800

    custom secret fields index in sql

--- src/main/java/stonegold/proxy/SqlSecretFieldsParser.java
+++ src/main/java/stonegold/proxy/SqlSecretFieldsParser.java
@@ -1,10 +1,33 @@
 package stonegold.proxy;
 
+import com.google.common.base.Function;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.primitives.Ints;
 
+import java.util.Collection;
+import java.util.List;
+
 public class SqlSecretFieldsParser {
+
+
     public SecretFields parse(String sql) {
+        // parse /*!secret in(2,3)*/
+        if (sql.startsWith("/*!secret")) {
+            int inStartPos = sql.indexOf("in(");
+            int inEndPos = sql.indexOf(')', inStartPos);
+            String fieldsIndex = sql.substring(inStartPos + 3, inEndPos);
+            List<String> split = Splitter.on(',').splitToList(fieldsIndex);
+            Collection<Integer> transform = Collections2.transform(split, new Function<String, Integer>() {
+                @Override
+                public Integer apply(String input) {
+                    return Integer.parseInt(input);
+                }
+            });
+            return new SecretFields(ImmutableSet.copyOf(transform));
+        }
+
         return new SecretFields(ImmutableSet.copyOf(Ints.asList(2, 3)));
     }
 }
--- src/test/java/stonegold/JdbcTest.java
+++ src/test/java/stonegold/JdbcTest.java
@@ -9,12 +9,24 @@ public class JdbcTest {
     public void simple() {
         Jdbc.execute("drop table if exists person");
         Jdbc.execute("create table  person(name varchar(10), id_no varchar(36), credit_card varchar(32))");
-        Jdbc.execute("insert into person(name, id_no, credit_card) values(?, ?, ?)",
+        Jdbc.execute("/*!secret in(2,3)*/insert into person(name, id_no, credit_card) values(?, ?, ?)",
                 "bingoo", "321421198312111234", "1111222233334444");
 
-        Person person = Jdbc.execute(Person.class, "select name, id_no, credit_card from person limit 1");
+        Person person = Jdbc.execute(Person.class,
+                "select name, id_no, credit_card from person where name = ?",
+                "bingoo");
 
         assertThat(person).isEqualTo(
                 new Person("bingoo", "secret:321421198312111234", "secret:1111222233334444"));
+
+        Jdbc.execute("/*!secret in(2)*/insert into person(name, id_no, credit_card) values(?, ?, ?)",
+                "dingoo", "321421198312111234", "1111222233334444");
+
+        Person person2 = Jdbc.execute(Person.class,
+                "select name, id_no, credit_card from person where name = ?",
+                "dingoo");
+
+        assertThat(person2).isEqualTo(
+                new Person("dingoo", "secret:321421198312111234", "1111222233334444"));
     }
 }
